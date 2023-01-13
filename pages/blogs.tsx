58c92797de326a357a715d653ac0c60ed631a1c2
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-13 10:45:53 +0700

    created blog card the simple first one

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,9 +1,31 @@
 import { NextPage } from "next";
+import BlogCard from "../component/BlogCard";
 
 interface Props {}
 
-const blogs: NextPage<Props> = () => {
-  return <div>blogs</div>;
+const Blogs: NextPage<Props> = () => {
+  return (
+    <div className="max-w-3xl mx-auto p-5 space-y-5">
+      <BlogCard
+        title="This is my blog title"
+        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempore
+      atque sequi veritatis corporis dolorum enim odio sunt praesentium
+      molestias."
+      />
+      <BlogCard
+        title="This is my blog title"
+        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempore
+      atque sequi veritatis corporis dolorum enim odio sunt praesentium
+      molestias."
+      />
+      <BlogCard
+        title="This is my blog title"
+        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore tempore
+      atque sequi veritatis corporis dolorum enim odio sunt praesentium
+      molestias."
+      />
+    </div>
+  );
 };
 
-export default blogs;
+export default Blogs;
