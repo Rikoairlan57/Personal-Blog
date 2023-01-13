87f47f9b3a3e5be113fbf0caa9b5e242f8ee4b12
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-13 17:49:39 +0700

    read data static

--- pages/api/posts.tsx
+++ pages/api/posts.tsx
@@ -1,13 +1,22 @@
 import { NextApiHandler } from "next";
+import fs from "fs";
+import path from "path";
 
 const handler: NextApiHandler = (req, res) => {
   const { method } = req;
 
   switch (method) {
-    case "GET":
-      return res.json({ ok: true });
+    case "GET": {
+      const data = readPostsInfo();
+      return res.json({ postInfo: data });
+    }
     default:
       return res.status(404).send("Not Found");
   }
+};
+
+const readPostsInfo = () => {
+  const dirPathToRead = path.join(process.cwd(), "posts");
+  return fs.readdirSync(dirPathToRead);
 };
 export default handler;
