f39b72d46549aa626ceeee15a9d6aecffc54c992
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-13 16:37:42 +0700

    next testing api

---
+++ pages/api/posts.tsx
@@ -0,0 +1,13 @@
+import { NextApiHandler } from "next";
+
+const handler: NextApiHandler = (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      return res.json({ ok: true });
+    default:
+      return res.status(404).send("Not Found");
+  }
+};
+export default handler;
