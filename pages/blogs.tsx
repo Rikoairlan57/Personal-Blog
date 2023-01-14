219d63feed31703ddb73a30977a895bba92e6313
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-14 11:06:12 +0700

    fixed fetching all fetch data slug

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -25,7 +25,12 @@ const Blogs: NextPage<Props> = ({ posts }) => {
   return (
     <div className="max-w-3xl mx-auto p-5 space-y-5">
       {posts.map((post) => (
-        <BlogCard title={post.title} desc={post.meta} key={post.title} />
+        <BlogCard
+          title={post.title}
+          desc={post.meta}
+          slug={post.slug}
+          key={post.slug}
+        />
       ))}
     </div>
   );
