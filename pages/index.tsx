65195d3e98f7a12f04671cf0d05f6a7ce485652f
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-15 06:13:38 +0700

    fixed destruc portfolis

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import { BlogList } from "../components/blogs";
-
+import { PortfolioList } from "../components/portfolios";
 import { Footer, Header, Navbar } from "../components/common";
 
 const portfolios = [
@@ -58,34 +58,7 @@ const Home: NextPage = () => {
               <a className="text-sm ml-1 text-indigo-600">(See All)</a>
             </Link>
           </h2>
-
-          {/* Portfolio List Starts */}
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {portfolios.map((portfolio) => (
-              <div key={portfolio.slug} className="group relative">
-                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-                  <Image
-                    layout="fill"
-                    src={portfolio.coverImage}
-                    alt={""}
-                    className="h-full w-full object-cover object-center"
-                  />
-                </div>
-                <h3 className="mt-6 text-sm text-gray-500">
-                  <Link href={`/portfolios/${portfolio.slug}`}>
-                    <a>
-                      <span className="absolute inset-0" />
-                      {portfolio.title}
-                    </a>
-                  </Link>
-                </h3>
-                <p className="text-base font-semibold text-gray-900">
-                  {portfolio.description}
-                </p>
-              </div>
-            ))}
-          </div>
-          {/* Portfolio List Ends */}
+          <PortfolioList />
         </div>
       </div>
       <Footer />
